b79bf2ca9633f39cdf423b1c1c760d07bbff2ff8
milshanfernando <milshanfernando.mf@gmail.com>
2025-09-28 23:43:19 +0400

    feat: update package structure and add SmartForm component with build scripts

---
+++ dist/cjs/SmartForm.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+declare const SmartForm: () => React.JSX.Element;
+export default SmartForm;
---
+++ dist/esm/SmartForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+const SmartForm = () => {
+    return React.createElement("div", null, "SmartForm");
+};
+export default SmartForm;
