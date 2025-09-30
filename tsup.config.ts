48de0e070dfb8801d99d89575bb549ba179e3737
milshanfernando <milshanfernando.mf@gmail.com>
2025-09-30 18:07:51 +0400

    fix: update tsup configuration for entry point and build process

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  entry: ["./src/index.ts"],
   dts: true,
   shims: true,
-  skipNodeModulesBundle: true,
   clean: true,
+  external: ["react", "react-dom"],
+  splitting: false,
+  sourcemap: true,
+  minify: true,
+  // Build Tailwind CSS when tsup finishes
+  onSuccess: "tailwindcss -i ./src/index.css -o ./dist/style.css --minify",
 });
