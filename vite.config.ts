0c2882edf7938b0098a4d58660fb606d59df5db5
milshanfernando <milshanfernando.mf@gmail.com>
2025-09-30 16:58:34 +0400

    fix: add build configuration for FormikYupSmartForm library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "FormikYupSmartForm",
+      fileName: (format) => `formik-yup-smartform.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], // don’t bundle React
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
